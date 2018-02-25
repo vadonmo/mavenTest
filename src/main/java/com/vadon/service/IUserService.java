8efbe4adad2f41348c4f9027d88247759b154a70
wwd <vadonmo@126.com>
2018-02-25 08:58:30 +0800

    first

---
+++ src/main/java/com/vadon/service/IUserService.java
@@ -0,0 +1,7 @@
+package com.vadon.service;
+
+import com.vadon.model.User;
+
+public interface IUserService {
+	public User getUserById(int id);
+}
---
+++ src/main/java/com/vadon/service/impl/IUserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.vadon.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.vadon.dao.IUserDao;
+import com.vadon.model.User;
+import com.vadon.service.IUserService;
+
+@Service("userService")
+public class IUserServiceImpl implements IUserService {
+
+	@Autowired
+	public IUserDao uDao;
+
+	public User getUserById(int id) {
+		return uDao.selectByPrimaryKey(id);
+	}
+
+}
